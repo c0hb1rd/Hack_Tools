70ab194492d8e70c1a3da53d4bdfcfa2f9b65623
c0hb1rd <cohbird.zn@gmail.com>
2016-03-12 17:24:07 +0800

    del

--- ckmd5.py
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python -c "print(('[*] The md5 value: ' + __import__('md5').md5(str(raw_input('[*] Input the source: '))).hexdigest()))"
--- ipable.py
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -n "[*] Input the range(xx.xx.nn.xx):"
-read range
-echo "[*] Now waitng..."
-nmap -sn 172.30.8$range.1-254 | grep -o --color=always "172\..*\..*\..*"
-echo -e "[*] The end."
